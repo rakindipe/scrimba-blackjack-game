648f90db473df19c4da0958fc17d2d3374af4967
Rakindipe <39384704+rakindipe@users.noreply.github.com>
2026-09-03 17:38:50 -0400

    add button to start game

--- main.js
+++ main.js
@@ -6,7 +6,19 @@ const getRandomInt = (min, max) => {
 let firstCard = getRandomInt(2, 11);
 let secondCard = getRandomInt(2, 11);
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message;
 
-console.log(firstCard);
-console.log(secondCard);
-console.log(sum);
+const startGame = () => {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game! 😭";
+    isAlive = false;
+  }
+  console.log(message);
+};
