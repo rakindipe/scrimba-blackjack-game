7b0f42b71944f61d0aebe27e381b585be799e106
Rakindipe <39384704+rakindipe@users.noreply.github.com>
2026-09-03 17:47:56 -0400

    add onclick ability to the "start game" btn

--- main.js
+++ main.js
@@ -9,16 +9,17 @@ let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message;
+let messageEl = document.querySelector("#message-el");
 
 const startGame = () => {
   if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
+    message = "Do you want to draw a new card?";
   } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
+    message = "You've got Blackjack!";
     hasBlackJack = true;
   } else {
-    message = "You're out of the game! 😭";
+    message = "You're out of the game!";
     isAlive = false;
   }
-  console.log(message);
+  messageEl.textContent = message;
 };
